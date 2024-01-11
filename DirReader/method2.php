77e1d855838636dddc93f990fa0ad72d005ce693
new92 <spyrospanagiotou1@gmail.com>
2024-01-11 14:21:48 +0200

    Update method2.php

--- DirReader/method2.php
+++ DirReader/method2.php
@@ -1,13 +1,19 @@
 <?php
 
 $items = scandir($_POST['dir']);
+$files = 0;
+$dirs = 0;
 for ($i = 0; $i < count($items); $i++) {
     echo $items[$i];
     if(is_file($items[$i])) {
         echo $items[$i] . ' | file<br>';
+        $files++;
     } else {
         echo $items[$i] . ' | directory<br>';
+        $dirs++;
     }
 }
+echo "Files ==> $files<br><br>";
+echo "Directories ==> $dirs<br><br>";
 
 ?>
