8da62584c1a3250ff6146e9253539db181fc8f5f
new92 <spyrospanagiotou1@gmail.com>
2024-01-11 14:01:49 +0200

    Create form.php

---
+++ DirReader/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DirReader</title>
+</head>
+<body>
+    <form method='post' action='method_.php'> <!-- Add method number to method.php . Example: method_1.php -->
+        Starting dir:
+        <input type='text' name='dir' />
+        <br /><br />
+        <input type='submit' name='submit' value='Submit' />
+    </form>
+</body>
+</html>
