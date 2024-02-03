6751bbb38168c57df37d8095bb5cf1e96e84284f
Spiros Panagiotou <spyrospanagiotou1@gmail.com>
2024-02-03 23:36:54 +0200

    Update localhost.php

--- Trooper/localhost.php
+++ Trooper/localhost.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = '127.0.0.1';
+$host = 'localhost';
 $port = 8080;
 
 echo "\n[RUNNING] Localhost is up and running on -> http://$host:$port";
 echo "\n[QUIT] Hit <Ctrl + C> to stop the server.\n";
-echo "[URL] To get started -> http://$host:$port/Trooper/form.html\n";
+echo "[URL] To get started -> http://$host:$port/form.html\n";
 exec('php -S localhost:' . $port);
