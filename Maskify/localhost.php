7d5fc18bc48c4107305d1caf92ff6f6a65d869cf
new92 <new92github@gmail.com>
2024-05-10 18:10:45 +0300

    minor changes

--- Maskify/localhost.php
+++ Maskify/localhost.php
@@ -7,5 +7,5 @@ $reset = "\033[0m";
 
 echo "\n" . $green . "[RUNNING] Localhost is up and running on -> http://$host:$port" . $reset;
 echo "\n[QUIT] Hit <Ctrl + C> to stop the server.\n";
-echo $green . "[URL] To get started -> http://$host:$port/main.html\n" . $reset;
+echo $green . "[URL] To get started -> http://$host:$port/index.html\n" . $reset;
 exec("php -S $host:$port");
